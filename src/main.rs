054bb9082e51fd3ce9d7b7ecea8c3c478fafb2bd
Alicja <alanotowska@yahoo.co.uk>
2024-10-01 22:48:35 +0200

    Changes to style for hover status and the button

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ fn main() -> iced::Result {
 
 struct BurnBabyBurn {
     text: String,
+    button_size: f32,
 }
 
 impl Default for BurnBabyBurn {
     fn default() -> Self {
         Self {
             text: String::from("Come on, press it... I know you want to!"),
+            button_size: 200.0,
         }
     }
 }
@@ -31,18 +33,19 @@ fn update(state: &mut BurnBabyBurn, message: Incinerator) {
     use Incinerator::*;
     if message == Incinerate {
         state.text = ("Burning....").to_string();
+        state.button_size *= 0.9;
         println!("Incinerate");
     }
 }
 
 fn view(state: &BurnBabyBurn) -> Element<Incinerator> {
     let square_with_text = container(column!["Burn,", "Baby Burn!"].align_x(Center))
-        .center_x(200)
-        .center_y(200);
-   
+        .align_x(Center)
+        .align_y(Center);
+
     let trigger = button(square_with_text)
-        .height(150)
-        .width(150)
+        .height(state.button_size)
+        .width(state.button_size)
         .on_press(Incinerator::Incinerate)
         .style(burning_style);
 
@@ -71,19 +74,30 @@ pub fn burning_style(_theme: &Theme, status: button::Status) -> button::Style {
             blur_radius: 15.0,
         },
         background: Some(Background::Color(Color::new(1.0, 0.0, 0.0, 1.0))),
-        ..Style::default()
     };
 
+    // DRY! - we only set stuff that's different from base style, and..
     let hovered_style = button::Style {
         text_color: Color::BLACK,
-        border: border::rounded(150),
         shadow: Shadow {
             color: Color::BLACK,
-            offset: Vector::new(5.0, 5.0),
-            blur_radius: 15.0,
+            offset: Vector::new(2.5, 2.5),
+            blur_radius: 10.0,
         },
         background: Some(Background::Color(Color::new(1.0, 0.0, 0.0, 0.90))),
+        ..base_style // ..the rest is as in base style
     };
+
+    // let hovered_style = button::Style {
+    //     text_color: Color::BLACK,
+    //     border: border::rounded(150),
+    //     shadow: Shadow {
+    //         color: Color::BLACK,
+    //         offset: Vector::new(5.0, 5.0),
+    //         blur_radius: 15.0,
+    //     },
+    //     background: Some(Background::Color(Color::new(1.0, 0.0, 0.0, 0.90))),
+    // };
 
     match status {
         // Status::Active | Status::Pressed | Status::Hovered => our_style,
